c5c0ba77a3e19138d11870ad2e3b5ed36a94f921
Yogaisan Ramasawmy <yogaisan@gmail.com>
2021-12-02 17:34:05 +0400

    created the radio component with a list of radio stations

---
+++ main.js
@@ -0,0 +1,31 @@
+const radioChannel = document.querySelector(".radio-channel")
+const radioFooterContent = document.querySelector(".radio-footer")
+
+fetch("https://teclead.de/recruiting/radios")
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+    data.radios.forEach((radio) => {
+      const radioContent = `
+      <div class="radios">
+        <div class="radio">
+          <h3 class="name">${radio.name}</h3>
+          <h3 class="frequency">${radio.frequency}</h3>
+        </div>
+        <div class="radio-image image-hide">
+          <i class="fas fa-minus-circle"></i>
+          <img src="${radio.image}">
+          <i class="fas fa-plus-circle"></i>
+        </div>
+      </div>`;
+
+      const footerContent = `
+      <div class="radio-footer image-hide">
+        <h4>CURRENTLY PLAYING</h4>
+        <p>${radio.name}</p>
+      </div>`;
+
+      radioChannel.insertAdjacentHTML("beforeend", radioContent);
+      radioFooterContent.insertAdjacentHTML("beforeend", footerContent)
+    });
+  });
